b135af983c67da5bd715fcd5846facabe6e82563
Mateo Cindrić <mateo.cindric0611@gmail.com>
2021-12-26 18:38:01 +0100

    Add test plugin for the prepared yang module

--- frontend/sysrepo-viewer/src/components/PageBody.jsx
+++ frontend/sysrepo-viewer/src/components/PageBody.jsx
@@ -1,31 +1,63 @@
 import { useState } from 'react'
 import ReactJson from 'react-json-view'
+import FadeLoader from "react-spinners/FadeLoader"
 import axios from 'axios'
-
-function editCallback(data) {
-
-}
-
-function deleteCallback(data) {
-
-}
-
-function addCallback(data) {
-
-}
 
 export default function PageBody() {
     const [path, setPath] = useState('')
     const [datastore, setDatastore] = useState('Running')
     const [viewerObject, setViewerObject] = useState({})
+    const [loading, setLoading] = useState(false)
 
-    async function getData(_) {
+    // /sysrepo-viewer-example:test-container
+
+    function editCallback(data) {
+        console.log(data)
+
+        // construct path
+        let valuePath = ""
+        for (const n of data.namespace) {
+            valuePath += "/" + n
+        }
+        valuePath += "/" + data.name
+
+        setLoading(true)
+
+        axios.post('http://localhost:5000/edit',
+            {
+                path: valuePath,
+                datastore,
+                value: data.new_value
+            })
+            .then(function (response) {
+                const newObject = viewerObject
+                newObject["sysrepo-viewer-example:test-container"]["valid"] = JSON.parse(data.new_value)
+                setViewerObject(newObject)
+                setLoading(false)
+            })
+            .catch(function (error) {
+            })
+
+        return false
+    }
+
+    function deleteCallback(data) {
+
+    }
+
+    function addCallback(data) {
+
+    }
+
+    async function getData(data) {
         console.log(path)
         console.log(datastore)
+        setLoading(true)
         const response = await axios.post('http://localhost:5000/', {
             path,
-            datastore,
+            datastore: datastore.toLowerCase(),
         })
+        setLoading(false)
         setViewerObject(response.data)
     }
 
@@ -54,9 +86,17 @@ export default function PageBody() {
                             <option>Operational</option>
                         </select>
                     </div>
-                    <button type="button" className="m-2 float-right justify-center py-2 px-4 text-sm font-medium rounded-none text-white bg-indigo-600" onClick={getData}>
-                        Get data
-                    </button>
+                    <div>
+                        <button type="button" className="m-2 justify-center py-2 px-4 text-sm font-medium rounded-none text-white bg-indigo-600" onClick={getData}>
+                            Get data
+                        </button>
+                    </div>
+
+                </div>
+                <div className="w-full flex justify-center">
+                    <div className="mx-auto">
+                        <FadeLoader color={'#4f46e5'} loading={loading} size={150} css={"margin-right: auto; margin-left: auto;"} />
+                    </div>
                 </div>
             </div>
             <div className="w-1/2 m-2 p-5 border-2 border-gray-400 text-gray-400 overflow-y-auto h-[45rem]">
@@ -64,13 +104,14 @@ export default function PageBody() {
                 <ReactJson src={viewerObject}
                     theme="ocean"
                     iconStyle="square"
-                    onEdit={datastore == 'Running' ? editCallback : false}
-                    onDelete={datastore == 'Running' ? deleteCallback : false}
-                    onAdd={datastore == 'Running' ? addCallback : false}
+                    onEdit={datastore === 'Running' ? editCallback : false}
+                    onDelete={datastore === 'Running' ? deleteCallback : false}
+                    onAdd={datastore === 'Running' ? addCallback : false}
                     displayDataTypes={false}
                     displayObjectSize={false}
                     indentWidth={4}
-                    collapseStringsAfterLength={20} />
+                    collapseStringsAfterLength={20}
+                    validationMessage={null} />
             </div>
         </div>
     )
